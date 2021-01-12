4e47bb7bcc86134350930ecc1cd5fd0d557f3589
Soldann <guotl321@gmail.com>
2021-01-12 00:28:12 -0500

    implement sending messages and ratelimiting

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ const RSS = require("rss-parser");
 const axios = require("axios");
 const Parser = require("rss-parser");
 
+const webhook = "WEBHOOK URL GOES HERE"
+
 let parser = new Parser();
-let time = new Date();
-console.log(time);
+let lastUpdateTime = new Date();
+console.log(lastUpdateTime);
+
+async function sendMessage(message, timeout){
+    axios.post(webhook, {
+        content: message
+    }).catch(function (err) {
+        if (err.response){
+            if (err.response.status == "429"){
+                console.log("429 error, ratelimiting!");
+                setTimeout(sendMessage.bind(null,message,timeout*2),timeout); //exponential backoff
+            }
+        }
+    });
+}
 
 async function getFeed() {
     let feed = await parser.parseURL("https://www.polygon.com/rss/nintendo/index.xml");
     feed.items.forEach(item => {
         console.log(item.title);
         let itemTime = new Date(item.pubDate);
-        console.log(itemTime < time);
+        if (lastUpdateTime < itemTime) {
+            sendMessage("*" + item.title + "*\n" + item.link, 1000);
+        }
     });
+
+    lastUpdateTime = new Date(); //update 
 }
 
 setInterval(getFeed,10000);
