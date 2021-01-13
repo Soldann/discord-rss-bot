2358443ab6dfbf0ce71bcb5f41c4d9da1cbcc010
Soldann <guotl321@gmail.com>
2021-01-13 18:31:34 -0500

    refactor to use rss-feed-emitter since it's more reliable than time-based

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 //const Discord = require("discord.js"); //don't technically need this, will use webhooks
-const RSS = require("rss-parser");
-const axios = require("axios");
-const Parser = require("rss-parser");
+const RssFeed = require("rss-feed-emitter");
+const axios = require("axios"); 
 
 
-const requestInterval = 10000;
+const feed = new RssFeed({ skipFirstLoad: true });
+feed.add({
+    url: "https://www.reddit.com/r/AskReddit/new/.rss",
+    refresh: 10000
+});
 
-let parser = new Parser();
-let lastUpdateTime = new Date();
-console.log(lastUpdateTime);
-
-async function sendMessage(message, timeout){
+async function sendMessage(message, timeout = 1000){
     axios.post(webhook, {
         content: message
     }).catch(function (err) {
@@ -23,20 +22,6 @@ async function sendMessage(message, timeout){
     });
 }
 
-async function getFeed() {
-    let feed = await parser.parseURL("https://www.reddit.com/r/AskReddit/new/.rss");
-    console.log("_________________________________________________________");
-    await Promise.all(feed.items.map(item => {
-        console.log(item.title);
-        let itemTime = new Date(item.pubDate);
-        console.log("last update: " + lastUpdateTime);
-        console.log("pub time: " + itemTime);
-        if (lastUpdateTime < itemTime) {
-            sendMessage("*" + item.title + "*\n" + item.link, 1000);
-        }
-    }));
-    lastUpdateTime = new Date(); //update 
-    console.log("Updated time: " + lastUpdateTime);
-}
-
-setInterval(getFeed,10000);
+feed.on('new-item', item => {
+    sendMessage("*" + item.title + "*\n" + item.link);
+});
